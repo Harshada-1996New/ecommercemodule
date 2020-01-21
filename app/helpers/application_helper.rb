4cecfefe1ca9106395075d17f6d678eb6c44ba65
Harshada-1996 <chincholeharshada96@gmail.com>
2020-01-21 11:54:43 +0530

    added multiple product at order creation time

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
-    def link_to_add_row(name,form,association,**args)
-        new_obj=form.object.send(association).klass.new
-        id=new_obj.object_id
-        fields=form.fields_for(association,new_obj,child_index:id) do |product_fields|
-         render(association.to_s.singularize,form:product_fields)
-        end
-        link_to(name,'#',class:"add_fields"+args[:class],data:{id:id,fields:fields.gsub("\n","")})
+    def link_to_add_fields(name, f, association)
+      new_object = f.object.send(association).klass.new
+      id = new_object.object_id
+      fields = f.fields_for(association, new_object, child_index: id) do |builder|
+        render(association.to_s.singularize + "_fields", f: builder)
+      end
+      link_to(name, '#', class: "add_fields_new", data: {id: id, fields: fields.gsub("\n", "")})
     end
 end
